36731514417fe6cf09c9eac2048e61e8a562e544
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-11-23 16:56:20 +0500

    json

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,25 @@ import './movies.json';
 import header from './assets/header-icon.png';
 import props from './movies.json';
 function App() {
-  const { id, Title, Year, Rated, Released, Runtime, Genre, Director, Image } =
-    props;
+  const [
+    {
+      Released,
+      id,
+      Awards,
+      Title,
+      Year,
+      Rated,
+      Runtime,
+      Genre,
+      Director,
+      Image,
+      Language,
+      Actors,
+      Country,
+      imdbRating,
+      Writer,
+    },
+  ] = props;
 
   return (
     <div>
@@ -38,46 +55,49 @@ function App() {
 
       <div className="cards">
         <div className="card-img container">
-          <img src="https://picsum.photos/200/300 " alt="" />
-          <h2>Avatar</h2>
-          <p>{id}</p>
+          <div className="images">
+            <img src={[Image]} alt="no img" />
+          </div>
+          <h2>{Title}</h2>
+          <p>{}</p>
         </div>
         <div className="card container card__container">
           <div className="card-first">
             <h2 className="card-year">Yil:</h2>
             <h3>{Year}</h3>
             <h2 className="release-date">Chiqarilgan sana</h2>
-            <h4>{Year}</h4>
-            <h2 className="genre">{Rated}</h2>
-            <h3>Action</h3>
+
+            <h4>{Released}</h4>
+            <h2 className="genre">Janr</h2>
+            <h3>{Genre}</h3>
             <h2 className="the-writer">Yozuvchi</h2>
-            <h3>{Released}</h3>
+            <h3>{Writer}</h3>
 
             <h2 className="language">Til</h2>
-            <h4>English</h4>
+            <h4>{Language}</h4>
 
             <h2 className="awards">Mukofotlar</h2>
-            <h4>sovga</h4>
+            <h4>{Awards}</h4>
           </div>
 
           <div className="card-two">
             <h2 className="rating">reyting</h2>
-            <h4>PG-13</h4>
+            <h4>{Rated}</h4>
 
             <h2 className="duration">Davomiyligi</h2>
-            <h4>162mln</h4>
+            <h4>{Runtime}</h4>
 
             <h2 className="rej">Rejissor</h2>
-            <h4>james</h4>
+            <h4>{Director}</h4>
 
             <h2 className="actors">Aktyorlar</h2>
-            <h4>john</h4>
+            <h4>{Actors}</h4>
 
             <h2 className="countries">Mamlakatlar</h2>
-            <h4>USA</h4>
+            <h4>{Country}</h4>
 
             <h2 className="IMDb-rating">IMDb reytingi</h2>
-            <h4>7.9</h4>
+            <h4>{imdbRating}</h4>
           </div>
         </div>
       </div>
